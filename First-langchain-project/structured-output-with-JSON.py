842d9e6d0789273606ad48e172980734a6b7adc2
Shri-LabelDev <shrikant.labelblind@gmail.com>
2026-07-17 23:03:36 +0530

    structured output with schema

---
+++ First-langchain-project/structured-output-with-JSON.py
@@ -0,0 +1,40 @@
+from langchain_openai import ChatOpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+model = ChatOpenAI(
+  model="gpt-5.6",
+  api_key=os.environ.get("MY_CUSTOM_KEY_VARIABLE"),
+  temperature=0,
+  )
+
+president_schema = {
+    "name": "president_info_schema",
+    "description": "Schema for extracting information about the president of a country",
+    "parameters": {
+      "type": "object",
+      "properties": {
+        "name": {
+          "type": "string",
+          "description": "The name of the president",
+        },
+        "country": {
+          "type": "string",
+          "description": "The country of the president",
+        },
+        "age": {
+          "type": ["integer", "null"],
+          "description": "The age of the president",
+        },
+      },
+      "required": ["name", "country"]
+
+    }
+}
+
+structured_llm = model.with_structured_output(president_schema)
+
+response = structured_llm.invoke("Who is the president of India?")
+
+print(response)
